9cf9b8eeb71c363b69fb8e4e2b643a649460a233
David Caboni <davcab+github@gmail.com>
2022-11-19 10:32:45 +0100

    First course : Hello John's world :)

---
+++ src/test/java/com/serenitydojo/HelloWorldWriteTest.java
@@ -0,0 +1,13 @@
+package com.serenitydojo;
+
+import org.junit.Test;
+
+public class HelloWorldWriteTest {
+
+    @Test
+    public void shouldWriteHelloWorldToTheConsole() {
+        HelloWorldWriter writer = new HelloWorldWriter();
+        writer.writeHelloWorld();
+    }
+
+}
